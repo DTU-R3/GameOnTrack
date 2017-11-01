288af9a2c276893f7f49b2a4a69c52b699fee310
Zhongyu Wang <zhongyuzone@gmail.com>
2017-11-01 11:15:55 +0100

    Add a console version running .net framework 4.7

---
+++ ROSOnTrack-Console/ROSOnTrack-Console/GameOnTrack.cs
@@ -0,0 +1,158 @@
+﻿using GOTSDK;
+using GOTSDK.Master;
+using GOTSDK.Master.Master2XTypes;
+using GOTSDK.Position;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace ROSOnTrack_Console
+{
+    class GameOnTrack
+    {
+        public static string calibPath = AppDomain.CurrentDomain.BaseDirectory + "Calibration.xml";
+
+        static public bool LoadCalib()
+        {
+            if(File.Exists(calibPath))
+            {
+                var doc = XDocument.Load(calibPath);
+                var loadedScenarios = Scenario3DPersistence.Load(doc);
+
+                foreach (var scenario in loadedScenarios)
+                    Program.scenarios.Add(scenario);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static void Close()
+        {
+            // Close the Master USB connection
+            if (Program.master != null)
+                Program.master.Close();
+        }
+
+        internal static bool Connect()
+        {
+            // Try to connect. This will return false immediately if it fails to find the proper USB port.
+            // Otherwise, it will begin connecting (async) and the "Master2X.OnMasterStatusChanged" event will be fired soon. 
+            return Program.master.BeginConnect();
+        }
+
+        static public void Init()
+        {
+            Program.master = new Master2X(SynchronizationContext.Current);
+            Program.master.OnMasterStatusChanged += OnMasterStatusChanged;
+            Program.master.OnNewReceiverConnected += OnNewReceiverConnected;
+            Program.master.OnNewTransmitterConnected += OnNewTransmitterConnected;
+            Program.master.OnMasterInfoReceived += OnMasterInfoReceived;
+            Program.master.OnMeasurementReceived += OnMeasurementReceived;
+        }
+
+        // Called when Master info has been received
+        private static void OnMasterInfoReceived(IMaster master, string swVersion, string serialNumber)
+        {
+            Program.masterVersion = string.Format("{0}, Serial: {1}", swVersion, serialNumber);
+        }
+
+        // This is called whenever the master connection status has been changed.
+        private static void OnMasterStatusChanged(IMaster master, MasterStatus newStatus)
+        {
+            Program.masterConnectionStatus = newStatus.ToString();
+
+            if (newStatus == MasterStatus.Connected)
+            {
+
+                // Display the name of the virtual COM port
+                Program.masterConnectionStatus += string.Format(" ({0})", master.CurrentPortName);
+
+                // Request firmware version and serial.
+                master.RequestMasterInfo();
+
+                // The current air temperature
+                int temperatureDegrees = 22;
+                ushort speedOfSoundInMeters = (ushort)(Master2X.GetSpeedOfSoundInMeters(temperatureDegrees) + 0.5);
+                ((Master2X)master).Setup(110, speedOfSoundInMeters, 16, 0, TPCLINK_ULTRASONIC_LEVEL.LEVEL_4);
+
+                // The very last thing: Request all currently connected units. They will show up in the OnNewTransmitter/OnNewReceiver connected events.
+                master.RequestUnits();
+            }
+        }
+
+        // Called when a new transmitter has been connected to Master. This includes the ones used in the calibrator triangle.
+        private static void OnNewTransmitterConnected(Transmitter transmitter)
+        {
+            if (!Program.connectedTransmitters.Any(t => t.GOTAddress == transmitter.GOTAddress))
+            {
+                Program.connectedTransmitters.Add(transmitter);
+                Program.master.SetTransmitterState(transmitter.GOTAddress, GetTransmitterState(transmitter.GOTAddress), Transmitter.UltraSonicLevel.High);
+            }
+        }
+
+        private static Transmitter.TransmitterState GetTransmitterState(GOTAddress gOTAddress)
+        {
+            if (CalibratorTriangle.IsCalibratorTriangleAddress(gOTAddress))
+                return Transmitter.TransmitterState.Deactivated;
+
+            return Transmitter.TransmitterState.ActiveHigh;
+        }
+
+        // Called when a new receiver has been connected to Master
+        private static void OnNewReceiverConnected(Receiver receiver)
+        {
+            if (!Program.connectedReceivers.Any(r => r.GOTAddress == receiver.GOTAddress))
+                Program.connectedReceivers.Add(receiver);
+        }
+
+        // Called when a measurement is received from the master
+        private static void OnMeasurementReceived(Measurement measurement)
+        {
+            if (Program.scenarios.Count == 0)
+            {
+                Console.WriteLine("GameOnTrack: No scenario found. ");
+            }
+            else if (measurement.RSSI == 0)
+            {
+                Console.WriteLine("GameOnTrack: Transmitter radio lost. ");
+            }
+            else if (measurement.RxMeasurements.Count(dist => dist.Distance > 0) < 3)
+            {
+                Console.WriteLine("GameOnTrack: The number of receivers is less than 3. ");
+            }
+            else
+            {
+                CalculatedPosition pos;
+                if (PositionCalculator.TryCalculatePosition(measurement, Program.scenarios.ToArray(), out pos))
+                {
+                    if( pos.TxAddress.ToString() == Program.sensor1.address.ToString() )
+                    {
+                        Program.sensor1.x = pos.Position.X;
+                        Program.sensor1.y = pos.Position.Y;
+                        Program.sensor1.z = pos.Position.Z;
+                    }
+                    if (pos.TxAddress.ToString() == Program.sensor2.address.ToString())
+                    {
+                        Program.sensor2.x = pos.Position.X;
+                        Program.sensor2.y = pos.Position.Y;
+                        Program.sensor2.z = pos.Position.Z;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("GameOnTrack: Can not calculate the position. ");
+                }
+            }
+        }       
+    }
+}
---
+++ ROSOnTrack-Console/ROSOnTrack-Console/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GOTSDK;
+using GOTSDK.Master;
+using GOTSDK.Position;
+using Ros_CSharp;
+using Messages.geometry_msgs;
+using Messages.std_msgs;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace ROSOnTrack_Console
+{
+    class Program
+    {
+        #region Variables
+
+        // Component configuration
+        static public GOTData sensor1 = new GOTData();
+        static public GOTData sensor2 = new GOTData();
+        static public double x = 0, y = 0, theta = 0;
+        static public double sensorDis = 100;
+        static public int GOTAddress1 = 41269;
+        static public int GOTAddress2 = 41272;
+
+        // GameOnTrack configuration
+        static public ObservableCollection<Transmitter> connectedTransmitters = new ObservableCollection<Transmitter>();
+        static public ObservableCollection<Receiver> connectedReceivers = new ObservableCollection<Receiver>();
+        static public ObservableCollection<Scenario3D> scenarios = new ObservableCollection<Scenario3D>();
+        static public Master2X master;
+        static public string masterConnectionStatus = "Offline";
+        static public string masterVersion = "Unknown";
+
+        // ROS .net variables
+        static Publisher<Vector3> sensor1Pub;
+        static Publisher<Vector3> sensor2Pub;
+        static Publisher<Pose2D> robotPosPub;
+        static Subscriber<Messages.std_msgs.Int32> addr1;
+        static Subscriber<Messages.std_msgs.Int32> addr2;
+        static NodeHandle nh;
+        static Vector3 data1_msg = new Vector3();
+        static Vector3 data2_msg = new Vector3();
+        static Pose2D result_msg = new Pose2D();
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            ROS.Init(new string[0], "GameOnTrack");
+            nh = new NodeHandle();
+            sensor1Pub = nh.advertise<Vector3>("/got1_data", 10);
+            sensor2Pub = nh.advertise<Vector3>("/got2_data", 10);
+            robotPosPub = nh.advertise<Pose2D>("/got_result", 10);
+            addr1 = nh.subscribe<Messages.std_msgs.Int32>("/GOT1Address", 1, Addr1CB);
+            addr2 = nh.subscribe<Messages.std_msgs.Int32>("/GOT2Address", 1, Addr2CB);
+            // Init component
+            sensor1.address = GOTAddress1;
+            sensor1.x = 0;
+            sensor1.y = 0;
+            sensor1.z = 0;
+            sensor2.address = GOTAddress2;
+            sensor2.x = 0;
+            sensor2.y = 0;
+            sensor2.z = 0;
+
+            // Init GameOnTrack
+            try
+            {
+                if (GameOnTrack.LoadCalib())
+                {
+                    Console.WriteLine("GameOnTrack calibration file found. ");
+                    GameOnTrack.Init();
+                    if (GameOnTrack.Connect())
+                    {
+                        Console.WriteLine("GameOnTrack master detected. ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Master can not be detected. ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Can not find GameOnTrack Calibration file. ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message.ToString());
+            }
+
+            Console.WriteLine("Starts to update GameOnTrack measurements. ");
+            while (true)
+            {
+                x = (sensor1.x + sensor2.x) / 2;
+                y = (sensor1.y + sensor2.y) / 2;
+                var theta_r = Math.Atan2(sensor2.y - sensor1.y, sensor2.x - sensor1.x) + Math.PI / 2;
+                theta_r = FitInRadius(theta_r);
+                theta = RadiusToDegree(theta_r);
+
+                // Publish the data to ROS
+                if (ROS.ok)
+                {
+                    data1_msg.x = Convert.ToInt32(sensor1.x);
+                    data1_msg.y = Convert.ToInt32(sensor1.y);
+                    data1_msg.z = Convert.ToInt32(sensor1.z);
+                    Console.WriteLine("Sensor 1 data, Address {0}:", sensor1.address);
+                    Console.WriteLine("\t X: {0}", data1_msg.x);
+                    Console.WriteLine("\t Y: {0}", data1_msg.y);
+                    Console.WriteLine("\t Z: {0}", data1_msg.z);
+
+                    data2_msg.x = Convert.ToInt32(sensor2.x);
+                    data2_msg.y = Convert.ToInt32(sensor2.y);
+                    data2_msg.z = Convert.ToInt32(sensor2.z);
+                    Console.WriteLine("Sensor 2 data, Address {0}:", sensor2.address);
+                    Console.WriteLine("\t X: {0}", data2_msg.x);
+                    Console.WriteLine("\t Y: {0}", data2_msg.y);
+                    Console.WriteLine("\t Z: {0}", data2_msg.z);
+
+                    result_msg.x = Convert.ToInt32(x);
+                    result_msg.y = Convert.ToInt32(y);
+                    result_msg.theta = theta_r;
+                    Console.WriteLine("Robot position:");
+                    Console.WriteLine("\t X: {0}", result_msg.x);
+                    Console.WriteLine("\t Y: {0}", result_msg.y);
+                    Console.WriteLine("\t Theta: {0}", result_msg.theta);
+
+                    sensor1Pub.publish(data1_msg);
+                    sensor2Pub.publish(data2_msg);
+                    robotPosPub.publish(result_msg);
+                }
+                else
+                {
+                    Console.WriteLine("ROS is not ready");
+                }
+
+                Thread.Sleep(100);
+            }          
+        }
+
+        private static void Addr1CB(Messages.std_msgs.Int32 argument)
+        {
+            GOTAddress1 = argument.data;
+            sensor1.address = GOTAddress1;
+        }
+
+        private static void Addr2CB(Messages.std_msgs.Int32 argument)
+        {
+            GOTAddress2 = argument.data;
+            sensor2.address = GOTAddress2;
+        }     
+
+        #region Math function
+
+        private static double FitInRadius(double r)
+        {
+            while (r < -Math.PI)
+            {
+                r = r + 2 * Math.PI;
+            }
+            while (r > Math.PI)
+            {
+                r = r - 2 * Math.PI;
+            }
+            return r;
+        }
+
+        private static double RadiusToDegree(double r)
+        {
+            var d = (r / Math.PI) * 180;
+            return d;
+        }
+
+        #endregion
+    }
+}
